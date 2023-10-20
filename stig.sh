8443400a240c6a98d79453db921bcc43046350a8
Morgan Metz <mlmi4@k12albemarle.org>
2023-10-20 09:56:12 -0400

    Create stig.sh

---
+++ stig.sh
@@ -0,0 +1,2 @@
+curl https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_STIGViewer-linux_x64-3-1-0.zip -o stig.zip
+curl https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_CAN_Ubuntu_20-04_LTS_V1R7_STIG_SCAP_1-2_Benchmark.zip -o scap.zip
